a678ed7d60fe01a1bcfdbd2a2d1c4d448846f77c
Wallace Vilela <wallace.vilela9@gmail.com>
2024-09-10 14:47:30 -0300

    Operacao de instanciacao da base de dados

---
+++ src/main/java/wvsdev/projetoapi/config/Instantiation.java
@@ -0,0 +1,25 @@
+package wvsdev.projetoapi.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+import wvsdev.projetoapi.domain.entities.User;
+import wvsdev.projetoapi.domain.repositories.UserRepository;
+
+import java.util.Arrays;
+
+@Configuration
+public class Instantiation implements CommandLineRunner {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        User maria = new User(null, "Maria Brown", "maria@gmail.com");
+        User alex = new User(null, "Alex Green", "alex@gmail.com");
+        User bob = new User(null, "Bob Grey", "bob@gmail.com");
+
+        userRepository.deleteAll();
+        userRepository.saveAll(Arrays.asList(maria, alex, bob));
+    }
+}
