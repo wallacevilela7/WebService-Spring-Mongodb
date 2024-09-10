11d0422bc4a8dcf587ab75522ba69c7b82699c8c
Wallace Vilela <wallace.vilela9@gmail.com>
2024-09-10 16:21:25 -0300

    Insercao de usuario com POST

--- src/main/java/wvsdev/projetoapi/domain/resources/UserResource.java
+++ src/main/java/wvsdev/projetoapi/domain/resources/UserResource.java
@@ -2,14 +2,13 @@ package wvsdev.projetoapi.domain.resources;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import wvsdev.projetoapi.domain.dto.UserDTO;
 import wvsdev.projetoapi.domain.entities.User;
 import wvsdev.projetoapi.domain.services.UserService;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -34,5 +33,13 @@ public class UserResource {
     public ResponseEntity<UserDTO> findById(@PathVariable String id) {
         User user = service.findById(id);
         return ResponseEntity.ok().body(new UserDTO(user));
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> insert(@RequestBody UserDTO userDTO) {
+        User obj = service.fromDTo(userDTO);
+        obj = service.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).build();
     }
 }
--- src/main/java/wvsdev/projetoapi/domain/services/UserService.java
+++ src/main/java/wvsdev/projetoapi/domain/services/UserService.java
@@ -2,6 +2,7 @@ package wvsdev.projetoapi.domain.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import wvsdev.projetoapi.domain.dto.UserDTO;
 import wvsdev.projetoapi.domain.entities.User;
 import wvsdev.projetoapi.domain.repositories.UserRepository;
 import wvsdev.projetoapi.domain.services.exception.ObjectNotFoundException;
@@ -20,5 +21,13 @@ public class UserService {
 
     public User findById(String id) {
         return repository.findById(id).orElseThrow(() -> new ObjectNotFoundException("User não encontrado"));
+    }
+
+    public User insert(User user){
+        return repository.insert(user);
+    }
+
+    public User fromDTo(UserDTO userDTO) {
+        return new User(userDTO.getId(), userDTO.getName(), userDTO.getEmail());
     }
 }
