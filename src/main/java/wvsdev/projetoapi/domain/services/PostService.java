1ed10505721abddb3d989dfca82704840d81e4ac
Wallace Vilela <wallace.vilela9@gmail.com>
2024-09-11 15:55:12 -0300

    Obtendo post por id

---
+++ src/main/java/wvsdev/projetoapi/domain/services/PostService.java
@@ -0,0 +1,18 @@
+package wvsdev.projetoapi.domain.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import wvsdev.projetoapi.domain.entities.Post;
+import wvsdev.projetoapi.domain.repositories.PostRepository;
+import wvsdev.projetoapi.domain.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository repository;
+
+    public Post findById(String id) {
+        return repository.findById(id).orElseThrow(() -> new ObjectNotFoundException("Post não encontrado"));
+    }
+}
