c53dda3f46a336b36a2fc33d8d1a1f39ce0b815d
Wallace Vilela <wallace.vilela9@gmail.com>
2024-09-11 15:06:43 -0300

    Referenciando os Posts do User

--- src/main/java/wvsdev/projetoapi/config/Instantiation.java
+++ src/main/java/wvsdev/projetoapi/config/Instantiation.java
@@ -40,5 +40,8 @@ public class Instantiation implements CommandLineRunner {
         Post p2 = new Post(null, Instant.parse("2024-03-21T23:13:45Z"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
 
         postRepository.saveAll(Arrays.asList(p1, p2));
+
+        maria.getPosts().addAll(Arrays.asList(p1, p2));
+        userRepository.save(maria);
     }
 }
